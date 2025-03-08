0148476c6fe219aa98dd9098f5d4766ab14d3a14
MK <99416359+MK316@users.noreply.github.com>
2025-03-08 23:42:27 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
 st.markdown("### 🌱 English Learning Application")
-st.write("Currently we have two apps to give pronunciation feedback based on WER (Word Error Rate) calculations")
+st.markdown("---")
+st.write("Giving pronunciation feedback based on WER (Word Error Rate) calculations: Currently, there are two menus on the left. One is for uploading recorded files, and the second is for users to record directly from their device.") 
+
+st.write("This is a demo version of the application, designed to showcase how it can serve as an automatic pronunciation feedback tool. It is planned to be updated later to suit the purposes of teachers.")
 st.caption("2025. 03. 08")
