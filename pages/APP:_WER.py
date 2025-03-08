be60dea23ebf4c93d09df597769eb73042607032
MK <99416359+MK316@users.noreply.github.com>
2025-03-08 17:38:41 +0900

    Update APP:_WER.py

--- pages/APP:_WER.py
+++ pages/APP:_WER.py
@@ -79,19 +79,19 @@ if st.button("Step 2. Display Feedback"):
     st.write("⭕ Your speech recognized as:", st.session_state['recognized_text'])
     st.markdown("---")
     if st.session_state['insertions']:
-        st.write("🔶 Insertion Errors:", ', '.join(st.session_state['insertions']))
+        st.write("⚪ Insertion Errors:", ', '.join(st.session_state['insertions']))
     else:
-        st.write("🔶 Insertion Errors: (None)")
+        st.write("⚪ Insertion Errors: (None)")
 
     if st.session_state['deletions']:
-        st.write("🔶 Deletion Errors:", ', '.join(st.session_state['deletions']))
+        st.write("⚪ Deletion Errors:", ', '.join(st.session_state['deletions']))
     else:
-        st.write("🔶 Deletion Errors: (None)")
+        st.write("⚪ Deletion Errors: (None)")
 
     if st.session_state['substitutions']:
-        st.write("🔶 Substitution Errors:", ', '.join(st.session_state['substitutions']))
+        st.write("⚪ Substitution Errors:", ', '.join(st.session_state['substitutions']))
     else:
-        st.write("🔶 Substitution Errors: (None)")
+        st.write("⚪ Substitution Errors: (None)")
     st.markdown("---")
     st.write("⭕ Word Error Rate (WER):", f"{st.session_state['wer']:.2f}%")
     st.markdown("---")
