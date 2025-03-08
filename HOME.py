a52dbfc91077b2f1eea63257d3e91d17adcdd29f
MK <99416359+MK316@users.noreply.github.com>
2025-03-08 23:36:17 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.caption("Test")
+st.markdown("English Learning Application")
+st.write("Currently we have two apps to give pronunciation feedback based on WER (Word Error Rate) calculations")
+st.caption("2025. 03. 08")
